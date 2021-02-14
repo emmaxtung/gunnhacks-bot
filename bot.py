8dfe26fe1b57f482c6bf7ee32e7a5c31c7ed11c3
emmaxtung <78984306+emmaxtung@users.noreply.github.com>
2021-02-14 06:38:35 -0600

    final commit
    some touchups, some comments

--- bot.py
+++ bot.py
@@ -2,19 +2,21 @@ import discord
 import random
 from discord.ext import commands
 
-client = commands.Bot(command_prefix="!")
+client = commands.Bot(command_prefix="!") #prefix for commands like !remove
 
 @client.event
 async def on_ready():
     print("Bot is alive")
-    await client.change_presence(activity=discord.Game(name='Watching for troublemakers'))
+    await client.change_presence(activity=discord.Game(name='Watching for troublemakers')) #fun status of the bot
 
+#purges an amount of messages
 @client.command()
 async def remove(ctx, amount=0):
     await ctx.channel.purge(limit=amount + 1)
     channel = client.get_channel(810409079481696299)
     await channel.send(str(amount) + " messages were removed.")
 
+#check to see if one of the blacklisted words is found in a user's message, and if so it will be removed
 @client.event
 async def on_message(message):
     blacklist = ["frick", "dang", "darn", "damn", "heck", "crap", "shizzle", "fudge"]
@@ -26,6 +28,7 @@ async def on_message(message):
             await channel.send(getRandomMessage(author_id))
     await client.process_commands(message)
 
+#this function allows the bot to send a snarky reply after a user swears
 def getRandomMessage(user_id):
     user_id = '<@' + str(user_id) + '>'
     responses = [
@@ -38,7 +41,6 @@ def getRandomMessage(user_id):
     ]
     reply = random.choice(responses)
     reply = reply.format(user_id)
-
     return reply
 
-client.run("secret token")
+client.run("SECRET TOKEN")
