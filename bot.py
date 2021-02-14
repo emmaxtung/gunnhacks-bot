9fc87f7459d2a7f48e0b2e15fed2288445efe430
emmaxtung <78984306+emmaxtung@users.noreply.github.com>
2021-02-14 05:26:16 -0600

    added bot responses to flagged msgs

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import discord
+import random
 from discord.ext import commands
 
 client = commands.Bot(command_prefix="!")
@@ -16,10 +17,28 @@ async def remove(ctx, amount=0):
 
 @client.event
 async def on_message(message):
-    blacklist = ["frick", "frickers", "motherfrickers", "motherfrick", "dang", "darn", "damn", "heck", "crap", "shizzle", "fudge"]
+    blacklist = ["frick", "dang", "darn", "damn", "heck", "crap", "shizzle", "fudge"]
+    channel = client.get_channel(810409079481696299)
+    author_id = message.author.id
     for word in blacklist:
         if(word in message.content):
             await message.delete()
+            await channel.send(getRandomMessage(author_id))
     await client.process_commands(message)
 
-client.run("SECRET TOKEN")
+def getRandomMessage(user_id):
+    user_id = '<@' + str(user_id) + '>'
+    responses = [
+        "{} is having a bad day.. poor thing.",
+        "{} woke up and chose violence today.",
+        "{}, was that really necessary?",
+        "{} thinks everyone  here is really amazing!",
+        "I heard soap was one of {}'s favorite things to eat!",
+        "Keep it PG, {}."
+    ]
+    reply = random.choice(responses)
+    reply = reply.format(user_id)
+
+    return reply
+
+client.run("ODEwNDA4NjExNTczNjYxNzI2.YCjN1Q.Nk_OTLEMYcew_NQubaLFkiCS4Tk")
